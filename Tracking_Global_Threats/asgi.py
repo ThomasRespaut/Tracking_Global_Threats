a971a4b16e628592d9822a6fcaeeb2faa71c1008
ThomasRespaut <thomas.respaut@edu.devinci.fr>
2024-04-22 15:56:34 +0200

    V1

---
+++ Tracking_Global_Threats/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Tracking_Global_Threats project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracking_Global_Threats.settings')
+
+application = get_asgi_application()
---
+++ Tracking_Global_Threats/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tracking_Global_Threats project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracking_Global_Threats.settings')
+
+application = get_wsgi_application()
